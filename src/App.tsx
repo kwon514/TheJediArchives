afe422e36420c7805cf736f899603a5e33e52516
kwon514 <109999814+kwon514@users.noreply.github.com>
2022-07-30 01:52:08 +1200

    implemented swapi api and material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useState } from "react";
+import SearchIcon from "@mui/icons-material/Search";
+import TextField from "@mui/material/TextField";
+import "./App.css";
+import { Box, Button, Grid, Paper, Skeleton } from "@mui/material";
 
 function App() {
+  const [characterName, setCharacterName] = useState("");
+  const [characterData, setCharacterData] = useState<undefined | any>(undefined);
+
+  const SWAPI_BASE_URL = "https://swapi.dev/api/";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+            <div className="search-field">
+        <h1>Pokédex Search</h1>
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <TextField
+            id="search-bar"
+            className="text"
+            value={characterName}
+            onChange={(prop) => {
+              setCharacterName(prop.target.value);
+            }}
+            label="Enter a Character Name..."
+            variant="outlined"
+            placeholder="Search..."
+            size="medium"
+          />
+          <Button
+            onClick={() => {
+              search();
+            }}
+          >
+            <SearchIcon style={{ fill: "blue" }} />
+            Search
+          </Button>
+        </div>
+      </div>
+
+      {characterData === undefined || characterData === null? (
+        <p>Character not found.</p>
+      ) : (
+        <div id="character-result">
+          <h1></h1>>
+        </div>
+      )}
     </div>
   );
-}
 
+  function search() {
+    axios.get(SWAPI_BASE_URL + "people/?search=" + characterName).then((res) => {
+      setCharacterData(res.data);
+    });
+  }
+}
 export default App;
