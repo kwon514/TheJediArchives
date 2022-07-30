fe6dc7cde49f56d0bf00b38b008335320e183d14
kwon514 <109999814+kwon514@users.noreply.github.com>
2022-07-30 18:41:58 +1200

    created basic page layout and displayed data

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import SearchIcon from "@mui/icons-material/Search";
 import TextField from "@mui/material/TextField";
 import "./App.css";
 import { Box, Button, Grid, Paper, Skeleton } from "@mui/material";
+import { red } from "@mui/material/colors";
 
 function App() {
   const [characterName, setCharacterName] = useState("");
@@ -21,7 +22,7 @@ function App() {
         <div id="text-box">
           <TextField id="search-bar" className="text" value={characterName} onChange={(prop) => { setCharacterName(prop.target.value); }} label="Enter a Character Name..." variant="outlined" placeholder="Search..." size="medium" />
           <Button onClick={() => { search(); }}>
-            <SearchIcon style={{ fill: "blue" }}/>
+            <SearchIcon style={{ fill: "blue" }} />
             Search
           </Button>
         </div>
@@ -31,9 +32,35 @@ function App() {
         <div></div>
       ) : (
         <div id="character-box">
-          <div className="character-info">
-            <h1>{characterData.results[0].name}</h1>
-          </div>
+          <Paper sx={{ backgroundColor: red }}>
+            <Grid item>
+              <Box>
+                {characterData === undefined || characterData === null ? (
+                  <h1> Character not found</h1>
+                ) : (
+                  <div>
+                    <h1>{characterData.results[0].name}</h1>
+                    <p className="data">
+                      Height: {(characterData.results[0].height) / 100} meters
+                      <br />
+                      Mass: {characterData.results[0].mass} kilograms
+                      <br />
+                      Hair Colour: {characterData.results[0].hair_color}
+                      <br />
+                      Skin Colour: {characterData.results[0].skin_color}
+                      <br />
+                      Eye Colour: {characterData.results[0].eye_color}
+                      <br />
+                      Birth Year: {characterData.results[0].birth_year}
+                      <br />
+                      Gender: {characterData.results[0].gender}
+                      <br />
+                    </p>
+                  </div>
+                )}
+              </Box>
+            </Grid>
+          </Paper>
         </div>
       )}
     </div>
